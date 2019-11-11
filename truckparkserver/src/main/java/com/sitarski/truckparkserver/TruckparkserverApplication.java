eae88667a484c8751e133a06a75a68b49d094a2a
Marek Sitarski <marek_sitarski@vp.pl>
2019-11-11 14:22:45 +0100

    add springboot confliguration folder

---
+++ truckparkserver/src/main/java/com/sitarski/truckparkserver/TruckparkserverApplication.java
@@ -0,0 +1,13 @@
+package com.sitarski.truckparkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckparkserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckparkserverApplication.class, args);
+	}
+
+}
---
+++ truckparkserver/src/test/java/com/sitarski/truckparkserver/TruckparkserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitarski.truckparkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckparkserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
