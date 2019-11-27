cdbfbec7f832601e211e670577b82e9fc95c70c6
marek sitarski <marek_sitarski@vp.pl>
2019-11-27 22:57:15 +0100

    Add sql inserts for drivers and mops coordinates #100004

---
+++ schema/insert_drivers.sql
@@ -0,0 +1,4 @@
+INSERT INTO driver(id, full_name, id_company) VALUES (1, 'Marek Sitarski', null);
+INSERT INTO driver(id, full_name, id_company) VALUES (1, 'Bogna Storma', null);
+INSERT INTO driver(id, full_name, id_company) VALUES (1, 'Maciej Sitarski', null);
+COMMIT:
