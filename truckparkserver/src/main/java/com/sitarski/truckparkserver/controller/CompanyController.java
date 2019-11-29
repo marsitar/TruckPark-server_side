f6604f3fdc487c7f99ef6521d5ce82725ef86323
marek sitarski <marek_sitarski@vp.pl>
2019-11-29 21:49:16 +0100

    Add controller layer for Company #100004

---
+++ truckparkserver/src/main/java/com/sitarski/truckparkserver/controller/CompanyController.java
@@ -0,0 +1,71 @@
+package com.sitarski.truckparkserver.controller;
+
+import com.sitarski.truckparkserver.domain.dto.CompanyDto;
+import com.sitarski.truckparkserver.service.CompanyService;
+import com.sitarski.truckparkserver.service.mapper.CompanyMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+
+    @Autowired
+    private final CompanyService companyService;
+
+    @Autowired
+    private final CompanyMapper companyMapper;
+
+    public CompanyController(CompanyService companyService, CompanyMapper companyMapper) {
+        this.companyService = companyService;
+        this.companyMapper = companyMapper;
+    }
+
+    @GetMapping(value = "/all", produces = "application/json")
+    public ResponseEntity<List<CompanyDto>> getAllCompanies() {
+
+        List<CompanyDto> companyDtoList = companyService.getCompanies();
+
+        return new ResponseEntity<>(companyDtoList, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/company/{id}", produces = "application/json")
+    public ResponseEntity<CompanyDto> getCompanyById(@PathVariable("id") Long id) {
+
+        CompanyDto companyDto = companyService.getCompanyById(id)
+                .orElse(null);
+
+        return new ResponseEntity<>(companyDto, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/company", consumes = "application/json")
+    public ResponseEntity<Object> addCompany(@Valid @RequestBody CompanyDto companyDto) {
+
+        companyService.addCompany(companyDto);
+
+        return new ResponseEntity<>(null, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/company", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<CompanyDto> updateCompany(@Valid @RequestBody CompanyDto companyDto) {
+
+        CompanyDto updatedCompanyDto = companyService.updateCompany(companyDto);
+
+        return new ResponseEntity<>(updatedCompanyDto, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/company/{id}", consumes = "text/plain")
+    public ResponseEntity<CompanyDto> deleteCompany(@PathVariable("id") Long id) {
+
+        companyService.deleteCompanyById(id);
+
+        return new ResponseEntity<>(null, new HttpHeaders(), HttpStatus.OK);
+    }
+
+}
--- truckparkserver/src/main/java/com/sitarski/truckparkserver/service/CompanyService.java
+++ truckparkserver/src/main/java/com/sitarski/truckparkserver/service/CompanyService.java
@@ -5,11 +5,13 @@ import com.sitarski.truckparkserver.domain.dto.CompanyDto;
 import com.sitarski.truckparkserver.domain.entity.Company;
 import com.sitarski.truckparkserver.service.mapper.CompanyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Service
 public class CompanyService {
 
     @Autowired
@@ -23,7 +25,7 @@ public class CompanyService {
         this.companyMapper = companyMapper;
     }
 
-    public List<CompanyDto> getCompanys() {
+    public List<CompanyDto> getCompanies() {
         return companyRepository
                 .findAll()
                 .stream()
