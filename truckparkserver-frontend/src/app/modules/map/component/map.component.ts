36b4eae966530afbc495bb1c43461e4ab4ac9ce8
marek sitarski <marek_sitarski@vp.pl>
2020-03-19 22:58:42 +0100

    Add Map component skeleton.

--- truckparkserver-frontend/src/app/modules/map/component/map.component.ts
+++ truckparkserver-frontend/src/app/modules/map/component/map.component.ts
@@ -1,15 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: '../component/map.component.html',
   styleUrls: ['../component/map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
+  private map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
   }
 
 }
