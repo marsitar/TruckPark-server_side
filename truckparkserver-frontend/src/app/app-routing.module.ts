9a76be689e62ef14478235aae95240976b341add
marek sitarski <marek_sitarski@vp.pl>
2020-04-08 21:35:39 +0200

    Add TruckPreviev Component and delete diver management component.

--- truckparkserver-frontend/src/app/app-routing.module.ts
+++ truckparkserver-frontend/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MapPreviewComponent} from './modules/map-preview/map-preview.component';
-import {DriverMaintainComponent} from './modules/driver-maintain/driver-maintain.component';
 import {DriverPrevievComponent} from './modules/driver-previev/driver-previev.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/driver-preview' },
   { path: 'map-preview', component: MapPreviewComponent },
-  { path: 'driver-maintain', component: DriverMaintainComponent },
   { path: 'driver-preview', component: DriverPrevievComponent },
 ];
 
---
+++ truckparkserver-frontend/src/app/modules/truck-previev/truck-previev.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-truck-previev',
+  templateUrl: './truck-previev.component.html',
+  styleUrls: ['./truck-previev.component.css']
+})
+export class TruckPrevievComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
