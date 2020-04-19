b6e912b36efdab2e789e99ef713ccaae3191cf56
marek sitarski <marek_sitarski@vp.pl>
2020-04-19 22:34:38 +0200

    Create a prototype of authentication and authorization system

--- truckparkserver/src/main/java/com/sitarski/truckparkserver/mapper/ExtendedMopDataMapper.java
+++ truckparkserver/src/main/java/com/sitarski/truckparkserver/mapper/ExtendedMopDataMapper.java
@@ -1,22 +1,13 @@
 package com.sitarski.truckparkserver.mapper;
 
-import com.sitarski.truckparkserver.configuration.ModelMapperConfiguration;
 import com.sitarski.truckparkserver.domain.dto.ExtendedMopDataDto;
 import com.sitarski.truckparkserver.domain.entity.ExtendedMopData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class ExtendedMopDataMapper implements Mapper<ExtendedMopDataDto, ExtendedMopData> {
-
-    private final ModelMapperConfiguration modelMapperConfiguration;
-
-    @Autowired
-    public ExtendedMopDataMapper(ModelMapperConfiguration modelMapperConfiguration){
-        this.modelMapperConfiguration = modelMapperConfiguration;
-    }
 
     @Override
     public ExtendedMopDataDto convertToDto(ExtendedMopData extendedMopData) {
